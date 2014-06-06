cf8106de605989089795114d97e26d4650c38aa5
Maxime Venin <maxime@sigmapix.com>
2014-06-06 17:57:07 +0200

    Typo
    Unable to found -> Unable to find

--- Model/BaseManager.php
+++ Model/BaseManager.php
@@ -52,7 +52,7 @@ abstract class BaseManager implements ManagerInterface
         $manager = $this->registry->getManagerForClass($this->class);
 
         if (!$manager) {
-            throw new \RuntimeException(sprintf("Unable to found the mapping information for the class %s."
+            throw new \RuntimeException(sprintf("Unable to find the mapping information for the class %s."
                 ." Please check the 'auto_mapping' option (http://symfony.com/doc/current/reference/configuration/doctrine.html#configuration-overview)"
                 ." or add the bundle to the 'mappings' section in the doctrine configuration.", $this->class));
         }
