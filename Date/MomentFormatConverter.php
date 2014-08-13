ef991bd231398b918fa0167e7a115c03da47fd61
Christian Morgan <github@caponica.com>
2014-08-13 15:58:45 +0100

    Added link to Moment formats
    To make it as easy as possible for other people to find the format syntax and build new mappings

--- Date/MomentFormatConverter.php
+++ Date/MomentFormatConverter.php
@@ -27,7 +27,8 @@ class MomentFormatConverter
 {
     /**
      * @var array This defines the mapping between PHP ICU date format (key) and moment.js date format (value)
-     *            For ICU formats: http://userguide.icu-project.org/formatparse/datetime#TOC-Date-Time-Format-Syntax
+     * For ICU formats see http://userguide.icu-project.org/formatparse/datetime#TOC-Date-Time-Format-Syntax
+     * For Moment formats see http://momentjs.com/docs/#/displaying/format/
      */
     private $phpMomentMapping = array(
         "yyyy-MM-dd'T'HH:mm:ssZZZZZ" => 'YYYY-MM-DDTHH:mm:ssZZ', // 2014-05-14T13:55:01+02:00
