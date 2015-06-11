69dbaaa50bec0a3cb1048f1812c13a06faa5380f
Sullivan SENECHAL <sullivan@nexylan.com>
2015-06-11 16:21:12 +0200

    Fix deprecated events

--- Form/EventListener/ResizeFormListener.php
+++ Form/EventListener/ResizeFormListener.php
@@ -63,8 +63,8 @@ class ResizeFormListener implements EventSubscriberInterface
     {
         return array(
             FormEvents::PRE_SET_DATA    => 'preSetData',
-            FormEvents::PRE_BIND        => 'preBind',
-            FormEvents::BIND            => 'onBind',
+            FormEvents::PRE_SUBMIT      => 'preBind',
+            FormEvents::SUBMIT          => 'onBind',
         );
     }
 
@@ -103,12 +103,28 @@ class ResizeFormListener implements EventSubscriberInterface
     }
 
     /**
+     * @param FormEvent $event
+     *
+     * @deprecated Since version 2.3, to be renamed in 3.0.
+     *             Use {@link preSubmit} instead
+     */
+    public function preBind(FormEvent $event)
+    {
+        // BC prevention for class extending this one.
+        if (get_called_class() !== 'Sonata\CoreBundle\Form\EventListener\ResizeFormListener') {
+            trigger_error(__CLASS__.'::'.__METHOD__.' is deprecated since 2.3 and will be renamed in 3.0. Use '.__CLASS__.'::preSubmit instead.', E_USER_DEPRECATED);
+        }
+
+        $this->preSubmit($event);
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormEvent $event
      *
      * @return mixed
      * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
      */
-    public function preBind(FormEvent $event)
+    public function preSubmit(FormEvent $event)
     {
         if (!$this->resizeOnBind) {
             return;
@@ -153,12 +169,28 @@ class ResizeFormListener implements EventSubscriberInterface
     }
 
     /**
+     * @param FormEvent $event
+     *
+     * @deprecated Since version 2.3, to be removed in 3.0.
+     *             Use {@link onSubmit} instead
+     */
+    public function onBind(FormEvent $event)
+    {
+        // BC prevention for class extending this one.
+        if (get_called_class() !== 'Sonata\CoreBundle\Form\EventListener\ResizeFormListener') {
+            trigger_error(__CLASS__.'::'.__METHOD__.' is deprecated since 2.3 and will be renamed in 3.0. Use '.__CLASS__.'::onSubmit instead.', E_USER_DEPRECATED);
+        }
+
+        $this->preSubmit($event);
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormEvent $event
      *
      * @return mixed
      * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
      */
-    public function onBind(FormEvent $event)
+    public function onSubmit(FormEvent $event)
     {
         if (!$this->resizeOnBind) {
             return;
