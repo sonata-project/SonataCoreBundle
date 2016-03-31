9bf7c93df81747a6165ef0c0ee26e005344b871d
Sebastian Pieczyński <testament777@gmail.com>
2016-03-31 13:23:49 +0200

    Update TranslatableChoiceType.php
    fix deprecation notice on getParent

--- Form/Type/TranslatableChoiceType.php
+++ Form/Type/TranslatableChoiceType.php
@@ -71,7 +71,8 @@ class TranslatableChoiceType extends AbstractType
      */
     public function getParent()
     {
-        return 'choice';
+        return class_exists('Symfony\Component\Form\Extension\Core\Type\ChoiceType') ?
+        'Symfony\Component\Form\Extension\Core\Type\ChoiceType' : 'choice';
     }
 
     /**
