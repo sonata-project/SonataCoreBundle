adce5fb414fd73e674260538294321512d08864d
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-04-22 14:08:21 +0200

    typo

--- Form/Type/BooleanType.php
+++ Form/Type/BooleanType.php
@@ -80,7 +80,7 @@ class BooleanType extends AbstractType
         if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')) {
             $choices = array_flip($choices);
 
-            // choice_as_value options is not needed of SF 3.0+
+            // choice_as_value options is not needed in SF 3.0+
             if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
                 $defaultOptions['choices_as_value'] = true;
             }
--- Form/Type/EqualType.php
+++ Form/Type/EqualType.php
@@ -62,7 +62,7 @@ class EqualType extends AbstractType
         if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')) {
             $choices = array_flip($choices);
 
-            // choice_as_value options is not needed of SF 3.0+
+            // choice_as_value options is not needed in SF 3.0+
             if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
                 $defaultOptions['choices_as_value'] = true;
             }
