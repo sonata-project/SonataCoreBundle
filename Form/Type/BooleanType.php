5ab932c027408e5765f65b3fc6f794c549a2467f
Fracsi <fracsi@gmail.com>
2016-04-29 13:39:23 +0200

    Fix typo in option name
    This typo was introduced in #238

--- Form/Type/BooleanType.php
+++ Form/Type/BooleanType.php
@@ -81,7 +81,7 @@ class BooleanType extends AbstractType
 
             // choice_as_value options is not needed in SF 3.0+
             if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
-                $defaultOptions['choices_as_value'] = true;
+                $defaultOptions['choices_as_values'] = true;
             }
         }
 
