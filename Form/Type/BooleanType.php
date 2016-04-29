sonata-project/SonataCoreBundle

commit 97d898bc190cf92e022535fcf0ffc7b1d5884e85

Flip choices depending on the version of Symfony (#238)

Form/Type/BooleanType.php
@@ -55,11 +55,12 @@ class BooleanType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'choices'            => array(
-                self::TYPE_YES  => 'label_type_yes',
-                self::TYPE_NO   => 'label_type_no',
-            ),
+        $choices =  array(
+            self::TYPE_YES => 'label_type_yes',
+            self::TYPE_NO  => 'label_type_no',
+        );
+
+        $defaultOptions = array(
             'transform' => false,
 
             // @deprecated Deprecated as of SonataCoreBundle 2.3.10, to be removed in 3.0.
@@ -73,7 +74,21 @@ class BooleanType extends AbstractType
 
                 return $options['translation_domain'];
             },
-        ));
+        );
+
+        // SF 2.7+ BC
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')) {
+            $choices = array_flip($choices);
+
+            // choice_as_value options is not needed in SF 3.0+
+            if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+                $defaultOptions['choices_as_value'] = true;
+            }
+        }
+
+        $defaultOptions['choices'] = $choices;
+
+        $resolver->setDefaults($defaultOptions);
     }
 
     /**

Tests/Form/Type/BooleanTypeTest.php
@@ -81,12 +81,19 @@ class BooleanTypeTest extends TypeTestCase
 
         $type->buildForm($builder, $resolvedOptions);
 
-        $this->assertEquals(array(
+        $expectedOptions = array(
             'transform'          => false,
             'catalogue'          => 'SonataCoreBundle',
             'translation_domain' => 'fooTrans',
             'choices'            => array(1 => 'foo_yes', 2 => 'foo_no'),
-        ), $resolvedOptions);
+        );
+
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')
+            && method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+            $expectedOptions['choices_as_value'] = true;
+        }
+
+        $this->assertEquals($expectedOptions, $resolvedOptions);
     }
 
     public function testLegacyDeprecatedCatalogueOption()
@@ -105,11 +112,18 @@ class BooleanTypeTest extends TypeTestCase
 
         $type->buildForm($builder, $resolvedOptions);
 
-        $this->assertEquals(array(
+        $expectedOptions = array(
             'transform'          => false,
             'catalogue'          => 'fooTrans',
             'translation_domain' => 'fooTrans',
             'choices'            => array(1 => 'foo_yes', 2 => 'foo_no'),
-        ), $resolvedOptions);
+        );
+
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')
+            && method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+            $expectedOptions['choices_as_value'] = true;
+        }
+
+        $this->assertEquals($expectedOptions, $resolvedOptions);
     }
 }

Form/Type/EqualType.php
@@ -51,12 +51,26 @@ class EqualType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'choices' => array(
-                self::TYPE_IS_EQUAL     => $this->translator->trans('label_type_equals', array(), 'SonataCoreBundle'),
-                self::TYPE_IS_NOT_EQUAL => $this->translator->trans('label_type_not_equals', array(), 'SonataCoreBundle'),
-            ),
-        ));
+        $choices = array(
+            self::TYPE_IS_EQUAL     => $this->translator->trans('label_type_equals', array(), 'SonataCoreBundle'),
+            self::TYPE_IS_NOT_EQUAL => $this->translator->trans('label_type_not_equals', array(), 'SonataCoreBundle'),
+        );
+
+        $defaultOptions = array();
+
+        // SF 2.7+ BC
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')) {
+            $choices = array_flip($choices);
+
+            // choice_as_value options is not needed in SF 3.0+
+            if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+                $defaultOptions['choices_as_value'] = true;
+            }
+        }
+
+        $defaultOptions['choices'] = $choices;
+
+        $resolver->setDefaults($defaultOptions);
     }
 
     /**

Tests/Form/Type/EqualTypeTest.php
@@ -20,7 +20,14 @@ class EqualTypeTest extends TypeTestCase
 {
     public function testGetDefaultOptions()
     {
-        $type = new EqualType($this->getMock('Symfony\Component\Translation\TranslatorInterface'));
+        $mock = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+
+        $mock->expects($this->exactly(2))
+            ->method('trans')
+            ->will($this->returnCallback(function ($arg) { return $arg; })
+            );
+
+        $type = new EqualType($mock);
 
         $this->assertEquals('sonata_type_equal', $type->getName());
         $this->assertEquals(
@@ -34,9 +41,20 @@ class EqualTypeTest extends TypeTestCase
 
         $options = $resolver->resolve();
 
+        $choices = array(1 => 'label_type_equals', 2 => 'label_type_not_equals');
+
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')) {
+            $choices = array_flip($choices);
+        }
+
         $expected = array(
-            'choices' => array(1 => null, 2 => null),
+            'choices' => $choices,
         );
+
+        if (method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')
+            && method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+            $expected['choices_as_value'] = true;
+        }
 
         $this->assertEquals($expected, $options);
     }

commit ccdb1c5d8fcf5c93ae1e643dea1d7d26510de8ab

Fix typo in option name
This typo was introduced in #238

Form/Type/BooleanType.php
@@ -81,7 +81,7 @@ class BooleanType extends AbstractType
 
             // choice_as_value options is not needed in SF 3.0+
             if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
-                $defaultOptions['choices_as_value'] = true;
+                $defaultOptions['choices_as_values'] = true;
             }
         }
 

Tests/Form/Type/BooleanTypeTest.php
@@ -84,14 +84,14 @@ class BooleanTypeTest extends TypeTestCase
         $expectedOptions = array(
             'transform'          => false,
             'catalogue'          => 'SonataCoreBundle',
-            'choices_as_value'   => true,
+            'choices_as_values'  => true,
             'translation_domain' => 'fooTrans',
             'choices'            => array(1 => 'foo_yes', 2 => 'foo_no'),
         );
 
         if (!method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')
             || !method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
-            unset($expectedOptions['choices_as_value']);
+            unset($expectedOptions['choices_as_values']);
         }
 
         $this->assertSame($expectedOptions, $resolvedOptions);
@@ -115,7 +115,7 @@ class BooleanTypeTest extends TypeTestCase
 
         $expectedOptions = array(
             'transform'          => false,
-            'choices_as_value'   => true,
+            'choices_as_values'  => true,
             'catalogue'          => 'fooTrans',
             'translation_domain' => 'fooTrans',
             'choices'            => array(1 => 'foo_yes', 2 => 'foo_no'),
@@ -123,7 +123,7 @@ class BooleanTypeTest extends TypeTestCase
 
         if (!method_exists('Symfony\Component\Form\AbstractType', 'configureOptions')
             || !method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
-            unset($expectedOptions['choices_as_value']);
+            unset($expectedOptions['choices_as_values']);
         }
 
         // "sort" trick can be remove when SF 2.3 support will be drop