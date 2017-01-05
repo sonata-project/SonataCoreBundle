8266ce781fcd7b8c2c9a45f3bc5bae4f55b442e4
Grégoire Paris <postmaster@greg0ire.fr>
2017-01-05 21:36:28 +0100

    Allow Twig 2.0

--- Tests/Twig/Node/TemplateBoxNodeTest.php
+++ Tests/Twig/Node/TemplateBoxNodeTest.php
@@ -31,7 +31,14 @@ class TemplateBoxNodeTest extends \Twig_Test_NodeTestCase
             'sonata_template_box'
         );
 
-        $this->assertSame(1, $body->getLine());
+        // Remove this when dropping twig/twig < 2.0
+        if (!method_exists($body, 'getTemplateLine')) {
+            $this->assertSame(1, $body->getLine());
+
+            return;
+        }
+
+        $this->assertSame(1, $body->getTemplateLine());
     }
 
     /**
