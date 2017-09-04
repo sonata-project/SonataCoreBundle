3b9c27dd64ce98781524842bcda219637d60513c
core23 <mail@core23.de>
2017-09-04 10:12:58 +0200

    Clean ManagerInterface

--- Model/ManagerInterface.php
+++ Model/ManagerInterface.php
@@ -11,8 +11,6 @@
 
 namespace Sonata\CoreBundle\Model;
 
-use Doctrine\DBAL\Connection;
-
 /**
  * @author Sylvain Deloux <sylvain.deloux@ekino.com>
  */
@@ -24,44 +22,6 @@ interface ManagerInterface
      * @return string
      */
     public function getClass();
-
-    /**
-     * Find all entities in the repository.
-     *
-     * @return array
-     */
-    public function findAll();
-
-    /**
-     * Find entities by a set of criteria.
-     *
-     * @param array      $criteria
-     * @param array|null $orderBy
-     * @param int|null   $limit
-     * @param int|null   $offset
-     *
-     * @return array
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
-
-    /**
-     * Find a single entity by a set of criteria.
-     *
-     * @param array      $criteria
-     * @param array|null $orderBy
-     *
-     * @return object|null
-     */
-    public function findOneBy(array $criteria, array $orderBy = null);
-
-    /**
-     * Finds an entity by its primary key / identifier.
-     *
-     * @param mixed $id The identifier
-     *
-     * @return object
-     */
-    public function find($id);
 
     /**
      * Create an empty Entity instance.
@@ -85,18 +45,4 @@ interface ManagerInterface
      * @param bool   $andFlush Flush the EntityManager after deleting the object?
      */
     public function delete($entity, $andFlush = true);
-
-    /**
-     * Get the related table name.
-     *
-     * @return string
-     */
-    public function getTableName();
-
-    /**
-     * Get the DB driver connection.
-     *
-     * @return Connection
-     */
-    public function getConnection();
 }
