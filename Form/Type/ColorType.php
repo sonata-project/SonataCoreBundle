08fe511dc792732f47d45e74cdde92005169130b
kunicmarko20 <kunicmarko20@gmail.com>
2017-09-09 14:19:23 +0200

    remove phpstorm header, add copyright and license information to ColorType

--- Form/Type/ColorType.php
+++ Form/Type/ColorType.php
@@ -1,9 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marko Kunic
- * Date: 9/9/17
- * Time: 11:23 AM
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Sonata\CoreBundle\Form\Type;
