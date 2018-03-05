e197bded351ac68269a5e442e0e7ee9503fcdb1f
Marko Kunic <kunicmarko20@gmail.com>
2018-03-05 22:14:16 +0100

    deprecate color type

--- src/Form/Type/ColorType.php
+++ src/Form/Type/ColorType.php
@@ -14,6 +14,19 @@ namespace Sonata\CoreBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+if (class_exists('Symfony\Component\Form\Extension\Core\Type\ColorType')) {
+    @trigger_error(
+        'The '.__NAMESPACE__.'\ColorType class is deprecated since version 3.x and will be removed in 4.0.'
+        .' Use Symfony\Component\Form\Extension\Core\Type\ColorType instead.',
+        E_USER_DEPRECATED
+    );
+}
+
+/**
+ * NEXT_MAJOR: remove this class.
+ *
+ * @deprecated since version 3.x, to be removed in 4.0. Use Symfony\Component\Form\Extension\Core\Type\ColorType instead
+ */
 final class ColorType extends AbstractType
 {
     public function getParent()
