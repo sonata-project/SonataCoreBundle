5c75303b15e04be7f694250754cf57a6ac577a31
core23 <mail@core23.de>
2018-12-08 17:05:33 +0100

    Revert: Moved form components to Twig namespace

---
+++ tests/CoreBundle/Twig/Extension/DeprecatedTextExtensionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\CoreBundle\Tests\Twig\Extension;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\CoreBundle\Twig\Extension\DeprecatedTextExtension;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+
+class DeprecatedTextExtensionTest extends TestCase
+{
+    /**
+     * @group legacy
+     * @expectedDeprecation Using the sonata.core.twig.extension.text service is deprecated since 3.2 and will be removed in 4.0
+     */
+    public function testDeprecation()
+    {
+        $extension = new DeprecatedTextExtension();
+        $extension->twig_truncate_filter(
+            new Environment(new ArrayLoader()),
+            'A long piece of text, well not that long actually but whatever.'
+        );
+    }
+}
---
+++ tests/CoreBundle/Twig/Extension/StatusExtensionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\CoreBundle\Tests\Twig\Extension;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\CoreBundle\Component\Status\StatusClassRendererInterface;
+use Sonata\CoreBundle\Twig\Extension\StatusExtension;
+
+class StatusExtensionTest extends TestCase
+{
+    public function testGetName()
+    {
+        $extension = new StatusExtension();
+        $this->assertSame('sonata_core_status', $extension->getName());
+    }
+
+    public function testGetFilters()
+    {
+        $extension = new StatusExtension();
+        $filters = $extension->getFilters();
+
+        $this->assertContainsOnlyInstancesOf('Twig_SimpleFilter', $filters);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testStatusClassDefaultValue()
+    {
+        $extension = new StatusExtension();
+        $statusService = $this->getMockBuilder(StatusClassRendererInterface::class)
+            ->getMock();
+        $statusService->expects($this->once())
+            ->method('handlesObject')
+            ->will($this->returnValue(false));
+
+        $extension->addStatusService($statusService);
+        $this->assertSame('test-value', $extension->statusClass(new \stdClass(), 'getStatus', 'test-value'));
+    }
+}
