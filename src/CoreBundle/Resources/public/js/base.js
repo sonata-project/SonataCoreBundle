26c6926c167bcf91c2c65c402caa93897433819b
Silas Joisten <silasjoisten@gmail.com>
2019-01-19 09:37:00 +0100

    removed inline css and replaced it with plain JavaScript

--- src/CoreBundle/Resources/public/js/base.js
+++ src/CoreBundle/Resources/public/js/base.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
     SonataCore.setup_select2(document);
     SonataCore.remove_iCheck_in_flashmessage();
+    SonataCore.addFlashmessageListener();
 });
 
 var SonataCore = {
@@ -39,5 +40,22 @@ var SonataCore = {
     },
     remove_iCheck_in_flashmessage: () => {
         jQuery('.read-more-state').iCheck('destroy');
+    },
+    addFlashmessageListener: () => {
+        document.querySelectorAll('.read-more-state').forEach((element) => {
+            element.addEventListener('change', (event) => {
+                let label = document.querySelector('label[for="' + element.id + '"]')
+                let labelMore = label.querySelector('.more')
+                let labelLess = label.querySelector('.less')
+
+                if (event.target.checked) {
+                    labelMore.classList.add('hide')
+                    labelLess.classList.remove('hide')
+                } else {
+                    labelMore.classList.remove('hide')
+                    labelLess.classList.add('hide')
+                }
+            });
+        })
     }
 };
